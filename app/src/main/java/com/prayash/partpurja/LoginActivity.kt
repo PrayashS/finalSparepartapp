c47c0a375de271b3dc809018b11398d182c7af36
PrayashS <sthaPrayash@gmail.com>
2021-01-22 20:05:35 +0545

    Setting setonclicker listner on login button

--- app/src/main/java/com/prayash/partpurja/LoginActivity.kt
+++ app/src/main/java/com/prayash/partpurja/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.prayash.partpurja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var btnlogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnlogin = findViewById(R.id.btnLogIn)
+
+        btnlogin.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
