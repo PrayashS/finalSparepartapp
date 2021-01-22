7c06fbd8a3d5a9ec5d3190069505a33361f2bcd9
PrayashS <sthaPrayash@gmail.com>
2021-01-22 20:07:38 +0545

    Setting set on clicklistner on register button

--- app/src/main/java/com/prayash/partpurja/RegisterActivity.kt
+++ app/src/main/java/com/prayash/partpurja/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.prayash.partpurja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var btnregister: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
+
+        btnregister = findViewById(R.id.btnRegister)
+
+        btnregister.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
